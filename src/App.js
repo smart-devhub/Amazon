9593582991b848ac61db55c48ac131ddd7dc115e
Zafar Ahmad <zafarswat9@gmail.com>
2021-12-12 02:35:50 +0500

    start

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      amazon
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAJaxE9h7180vj7XclEoA3xaEPoA7Uq_p4",
+    authDomain: "challenge-1c095.firebaseapp.com",
+    projectId: "challenge-1c095",
+    storageBucket: "challenge-1c095.appspot.com",
+    messagingSenderId: "127393924829",
+    appId: "1:127393924829:web:08619b3d8e47d7f2d13d34"
+  };
